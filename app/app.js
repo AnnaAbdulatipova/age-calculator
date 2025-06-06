e9346bec18e7b786aebacc7b79ea92464789ac04
AnnaAbdulatipova <78266580+AnnaAbdulatipova@users.noreply.github.com>
2025-06-06 14:15:11 +0300

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var name = prompt('Hi! :) What is your name?');
+alert('Nice to meet you, ' + name);
+
+var dob = prompt('What year were you born?');
+var yearTill = prompt('What year till you want to calculate your age?');
+
+alert(yearTill - dob);
